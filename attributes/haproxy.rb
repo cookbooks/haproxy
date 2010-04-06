79c11ac2dfb44f5684c6e3ef9fb2e5922b8817d6
Joshua Sierles <jsierles@gmail.com>
2010-04-06 14:52:29 +0200

    major updates for 0.8 compatibility. some recipes now use databags by default.

--- attributes/haproxy.rb
+++ attributes/haproxy.rb
@@ -1,4 +1,3 @@
-haproxy Mash.new unless attribute?(:haproxy)
-haproxy[:user] = "app" unless haproxy.has_key?(:user)
-haproxy[:group] = "app" unless haproxy.has_key?(:group)
-haproxy[:instances] = [] unless haproxy.has_key?(:instances)
+default.haproxy[:user] = "app"
+default.haproxy[:group] = "app"
+default.haproxy[:instances] = []
